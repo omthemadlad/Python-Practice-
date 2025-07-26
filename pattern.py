6c4334bccf06fffa42ddbc5f556acfa208fb4eb5
OMKAR <omkaar.work@gmail.com>
2025-07-26 09:56:01 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,8 @@
+for i in range(1,6):
+    spaces = 5-i
+    stars = i * " *"
+    print(" "*spaces + stars)
+for i in range(5,0,-1):
+    spaces = 5-i
+    stars = i * " *"
+    print(" "*spaces + stars)
